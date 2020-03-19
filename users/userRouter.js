d96e85b2d0b75b44d1fbc186fcaabe3fab3abf29
Jeff Breig <jjbreig@gmail.com>
2020-03-19 14:48:33 -0400

    crud operations semi-complete

--- users/userRouter.js
+++ users/userRouter.js
@@ -28,24 +28,81 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+  .then(user=>{
+    res.status(200).json(user);
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({
+      message:'error getting user data'
+    });
+  });
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  Users.getUserPosts(req.params.id)
+  .then(posts=>{
+    res.status(200).json(posts);
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({message:'error getting user posts'})
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+  .then(count=>{
+    if(count>0){
+      res.status(200).json({message: 'user deleted'});
+    } else{
+      res.status(404).json({message: "couldn't find user"})
+    }
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({message: 'there was an error removing the user', error})
+  });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', [validateUserId, validatePost], (req, res) => {
   // do your magic!
+  Users.update(req.params.id, req.body)
+  .then(user=>{
+    if(user){
+      res.status(200).json(user);
+    } else{
+      res.status(404).json({message:"error finding the user"})
+    }
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({message:"error updating the user"})
+  });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        // res.status(404).json({ message: 'does not exist' });
+        next(new Error('does not exist'));
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'exception', err });
+    });
 }
 
 function validateUser(req, res, next) {
