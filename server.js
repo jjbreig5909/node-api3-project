c192c62d6bf763adb1b70d543a0615c79dd500ec
Jeff Breig <jjbreig@gmail.com>
2020-03-19 19:42:44 -0400

    mvp

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter=require('./users/userRouter.js');
 const server = express();
-
+server.use(express.json());
 server.use(logger);
 server.use('/api/users', userRouter);
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,15 +1,45 @@
 const express = require('express');
 
 const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the user',
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', [validatePost, validateUserId], (req, res) => {
   // do your magic!
+  const { text } = req.body;
+  const user_id = req.params.id;
+
+  Users.getById(user_id)
+    .then(post => {
+        let newPost = {
+          text,
+          user_id,
+        }
+
+        Posts.insert(newPost)
+          .then(post => {
+            res.status(201).json({ message: "post created" })
+          })
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'I cannot provide any info from the inner server, try again!', err })
+    })
 });
 
 router.get('/', (req, res) => {
@@ -26,23 +56,15 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-  Users.getById(req.params.id)
-  .then(user=>{
-    res.status(200).json(user);
-  })
-  .catch(error=>{
-    console.log(error);
-    res.status(500).json({
-      message:'error getting user data'
-    });
-  });
+
+router.get('/:id', validateUserId, (req, res) => {
+      res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
-  Users.getUserPosts(req.params.id)
+  Users.getUserPosts(req.user.id)
   .then(posts=>{
     res.status(200).json(posts);
   })
@@ -52,7 +74,7 @@ router.get('/:id/posts', (req, res) => {
   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
   Users.remove(req.params.id)
   .then(count=>{
@@ -68,8 +90,9 @@ router.delete('/:id', (req, res) => {
   });
 });
 
-router.put('/:id', [validateUserId, validatePost], (req, res) => {
+router.put('/:id',validateUserId, (req, res) => {
   // do your magic!
+  console.log(req.body);
   Users.update(req.params.id, req.body)
   .then(user=>{
     if(user){
@@ -91,8 +114,11 @@ function validateUserId(req, res, next) {
   const { id } = req.params;
   Users.getById(id)
     .then(user => {
+      console.log(user);
       if (user) {
+        
         req.user = user;
+        req.user.id=user.id;
         next();
       } else {
         // res.status(404).json({ message: 'does not exist' });
@@ -107,10 +133,27 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // do your magic!
+  console.log(req.body);
+  const body = req.body;
+  if (Object.entries(req.body).length === 0) {
+    res.status(400).json({ message: 'Please include request body' })
+  }
+  else if (!req.body.name) {
+    res.status(400).json({ message: "missing a name" })
+  }
+    next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  console.log(req.body);
+  const body = req.body;
+  if(!body || body === {}){
+    res.status(400).json({ message: 'Please include request body' })}
+  else if (!req.body.text){
+    res.status(400).json({message: "missing some text"})
+  }
+    next();
 }
 
 module.exports = router;
